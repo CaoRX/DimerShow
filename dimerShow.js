6c490ca85b6cdc3ecc868aa70b05408352863222
Nan <umi@pku.edu.cn>
2021-06-02 10:11:55 +0900

    add direct loop update, add physical parameters

--- dimerShow.js
+++ dimerShow.js
@@ -1,12 +1,17 @@
 'use strict';
 
-console.log("Hello world!");
+// console.log("Hello world!");
+
+function randomInt(n) {
+    return Math.floor(Math.random() * n);
+}
 
 class State {
     constructor(n) {
         this.n = n;
         this.buildLattice();
         this.initState();
+        this.setPhysicalParameters();
         // this.initHorizontalState();
         this.initContainer();
         // this.container = $("#lattice");
@@ -21,6 +26,37 @@ class State {
         // this.repaint();
         // console.log(this.hDimer);
         // console.log("FP =", this.FPNum);
+    }
+
+    setPhysicalParameters(V = "-1", T = "inf") {
+        if (T == "inf") {
+            this.beta = 0.0; // infinite T
+            this.infBeta = false;
+        } else {
+            let TVal = parseFloat(T);
+            if (TVal < 1e-8) {
+                this.infBeta = true;
+            } else {
+                this.infBeta = false;
+                this.beta = 1.0 / TVal;
+            }
+        }
+        this.V = parseFloat(V);
+
+        this.showPhysicalParameters();
+    }
+    getTemperature() {
+        if (this.infBeta) {
+            return 0.0;
+        } else if (this.beta < 1e-8) {
+            return "inf";
+        } else {
+            return 1.0 / this.beta;
+        }
+    }
+    showPhysicalParameters() {
+        document.getElementById('V-input').value = this.V;
+        document.getElementById('T-input').value = this.getTemperature();
     }
 
     buildLattice() {
@@ -322,6 +358,125 @@ class State {
             }
         }
     }
+
+    pushToQueue(x, y) {
+        x = this.mod(x); y = this.mod(y);
+        this.queue.push({'x': x, 'y': y});
+        this.siteIndexInQueue[x][y] = this.queue.length - 1;
+    }
+
+    getNextCoord(x, y, nextDimer) {
+        let xx = 0, yy = 0;
+        if (nextDimer) {
+            if (this.getHDimer(x, y)) {
+                xx = x; yy = this.mod(y + 1);
+            } else if (this.getHDimer(x, y - 1)) {
+                xx = x; yy = this.mod(y - 1);
+            } else if (this.getVDimer(x, y)) {
+                xx = this.mod(x + 1); yy = y;
+            } else if (this.getVDimer(x - 1, y)) {
+                xx = this.mod(x - 1); yy = y;
+            } else {
+                console.log(`Error: site (${x}. ${y}) does not linke to a dimer.`);
+            }
+        } else {
+            let choices = [];
+            if (!this.getHDimer(x, y)) {
+                choices.push([x, this.mod(y + 1)]);
+            } 
+            if (!this.getHDimer(x, y - 1)) {
+                choices.push([x, this.mod(y - 1)]);
+            } 
+            if (!this.getVDimer(x, y)) {
+                choices.push([this.mod(x + 1), y]);
+            } 
+            if (!this.getVDimer(x - 1, y)) {
+                choices.push([this.mod(x - 1), y]);
+            }
+            let idx = randomInt(choices.length);
+            xx = choices[idx][0]; yy = choices[idx][1];
+        }
+        return {'x': xx, 'y': yy};
+    }
+
+    flipBondBetween(x1, y1, x2, y2) {
+        if (x1 == x2) {
+            if (this.mod(y1 + 1) == y2) {
+                this.flipHDimer(x1, y1);
+            } else {
+                this.flipHDimer(x1, y2);
+            }
+        } else if (y1 == y2) {
+            if (this.mod(x1 + 1) == x2) {
+                this.flipVDimer(x1, y1);
+            } else {
+                this.flipVDimer(x2, y1);
+            }
+        } else {
+            console.log(`Error in flipBondBetween: (${x1}, ${y1}) and (${x2}, ${y2}) are not neighbors.`);
+        }
+    }
+
+    loopFlip(startIndex) {
+        for (let i = startIndex; i < this.queue.length - 1; ++i) {
+            // console.log(this.queue[i], this.queue[i + 1]);
+            this.flipBondBetween(this.queue[i]['x'], this.queue[i]['y'], this.queue[i + 1]['x'], this.queue[i + 1]['y']);
+        }
+        let qn = this.queue.length;
+        this.flipBondBetween(this.queue[qn - 1]['x'], this.queue[qn - 1]['y'], this.queue[startIndex]['x'], this.queue[startIndex]['y']);
+    }
+
+    decideAccept(oldFP, newFP) {
+        if (this.infBeta) {
+            // if (V > 0) : reject increase, accept decrease(and no change)
+            if (this.V > 0) {
+                return (newFP <= oldFP);
+            } else {
+                return (newFP >= oldFP);
+            }
+        } else {
+            let deltaE = this.V * (newFP - oldFP);
+            let poss = Math.exp(-this.beta * deltaE);
+            // console.log("poss =", poss);
+            // console.log(`oldFP = ${oldFP}, newFP = ${newFP}, deltaE = ${deltaE}`);
+            return Math.random() < poss;
+        }
+    }
+
+    directLoop() {
+        // console.log(this);
+        if (!this.siteIndexInQueue) {
+            this.siteIndexInQueue = Array(this.n);
+            for (let i = 0; i < this.n; ++i) {
+                this.siteIndexInQueue[i] = Array(this.n);
+                for (let j = 0; j < this.n; ++j) {
+                    this.siteIndexInQueue[i][j] = -1;
+                }
+            }
+        }
+        this.queue = [];
+        let oldFP = this.countFP();
+        let sx = randomInt(this.n), sy = randomInt(this.n);
+        let cx = sx, cy = sy;
+        let nextDimer = true;
+        do {
+            this.pushToQueue(cx, cy);
+            let nextCoord = this.getNextCoord(cx, cy, nextDimer);
+            cx = nextCoord['x']; cy = nextCoord['y'];
+            nextDimer = !nextDimer;
+        } while (this.siteIndexInQueue[cx][cy] == -1); 
+        // console.log("queue,", this.queue);
+        let startIndex = this.siteIndexInQueue[cx][cy];
+        this.loopFlip(startIndex);
+        let newFP = this.countFP();
+        if (!this.decideAccept(oldFP, newFP)) {
+            this.loopFlip(startIndex);
+        }
+        for (let i = 0; i < this.queue.length; ++i) {
+            this.siteIndexInQueue[this.queue[i]['x']][this.queue[i]['y']] = -1;
+        }
+        this.repaint();
+    }
 }
 
 function initDimerState(n) {
@@ -372,6 +527,18 @@ resizeButton.addEventListener("click", () => {
         resize(newSize);
     }
 });
+
+let directLoopButton = document.getElementById("direct-loop");
+directLoopButton.addEventListener("click", () => {
+    state.directLoop();
+});
+
+let physicalSubmitButton = document.getElementById("physical");
+physicalSubmitButton.addEventListener("click", () => {
+    let V = document.getElementById("V-input").value;
+    let T = document.getElementById("T-input").value;
+    state.setPhysicalParameters(V, T);
+})
 
 // console.log($("#lattice"));
 
